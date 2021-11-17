11ad82e8eabb03e40e0ac1cfc0109940090c46a6
Mark Giannelli <giannelli.mark@gmail.com>
2021-11-17 12:17:48 -0600

    create Employees table and columns in our sample database

--- sql_graphql/backend/models.py
+++ sql_graphql/backend/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+# class is equivalent to a table
+# the keys are equivalent to a column
+class Employee(models.Model):
+    f_name = models.CharField(max_length=50)
+    l_name = models.CharField(max_length=50)
