42053d2bc230fea348c687e179e596ab98b280b7
Mark Giannelli <giannelli.mark@gmail.com>
2021-11-17 13:50:29 -0600

    add functionality to insert a new employee into the db and then fetch the db info and render it to screen

--- sql_graphql/backend/views.py
+++ sql_graphql/backend/views.py
@@ -1,6 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Employee
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    employees = Employee.objects.all()
+
+    return render(request, 'index.html', {'employees': employees})
+
+def add(request):
+    context = {}
+
+    if request.method == 'POST':
+        firstname = request.POST.get('firstname')
+        lastname = request.POST.get('lastname')
+
+        try: 
+            obj = Employee(f_name=firstname, l_name=lastname)
+            # this inserts a row into the database
+            obj.save()
+            return redirect('/')
+        except ValueError as e:
+            context['error'] = e
+    return render(request, 'add.html', context)
